981e4f021441d32458c153ba53e8fad0f30869e1
Atzac <isaacquidutemelo13@gmail.com>
2022-08-03 09:48:38 -0300

    functions

--- src/app/modules/home/components/input-add-items/input-add-items.component.ts
+++ src/app/modules/home/components/input-add-items/input-add-items.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-input-add-items',
-  templateUrl: './input-add-items.component.html',
-  styleUrls: ['./input-add-items.component.scss']
+  selector: "app-input-add-items",
+  templateUrl: "./input-add-items.component.html",
+  styleUrls: ["./input-add-items.component.scss"],
 })
 export class InputAddItemsComponent implements OnInit {
+  @Output() public emmitItem = new EventEmitter();
+  public addItem: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public submitItem() {
+    this.addItem = this.addItem.trim(); //remove espaços
+    if (this.addItem) {
+      this.emmitItem.emit(this.addItem);
+      this.addItem = '';
+    }
   }
-
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,17 +1,33 @@
-import { TaskList } from './../../model/task-list';
-import { Component, OnInit } from '@angular/core';
+import { TaskList } from "./../../model/task-list";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-todo-list',
-  templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  selector: "app-todo-list",
+  templateUrl: "./todo-list.component.html",
+  styleUrls: ["./todo-list.component.scss"],
 })
 export class TodoListComponent implements OnInit {
-  public taskList: Array<TaskList> = []
+  public taskList: Array<TaskList> = [
+    { taskName: "teste 1", checked: false },
+    { taskName: "teste 1", checked: false },
+    { taskName: "teste 1", checked: false },
+    { taskName: "teste 1", checked: false },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public deleteItem(idx: number) {
+    this.taskList.splice(idx, 1);
   }
 
+  public deleteAll() {
+    const confirm = window.confirm("Deseja remover tudo?");
+    if (confirm) this.taskList = [];
+  }
+
+  public setEmmitedTaskItem(taskEmmited: string) {
+    this.taskList.push({ taskName: taskEmmited, checked: false });
+  }
 }
